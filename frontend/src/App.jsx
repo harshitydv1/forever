7bb660f4afe87218e55cc55c5d5b8d1631d10c5e
Harshit Raj <harshit.raj@adypu.edu.in>
2026-03-27 01:39:16 +0530

    Fix import case for Home component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Routes, Route, Navigate } from 'react-router-dom'
-import Home from './pages/home.jsx'
+import Home from './pages/Home.jsx'
 import About from './pages/About.jsx'
 import Contact from './pages/Contact.jsx'
 import Collection from './pages/Collection.jsx'
